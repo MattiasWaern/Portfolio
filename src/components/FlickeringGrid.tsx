db90e180885d6209747f7da37a1119c8cba83081
ReggeMeister <80971552+MattiasWaern@users.noreply.github.com>
2026-09-13 16:35:00 +0200

    Update FlickeringGrid.tsx

--- src/components/FlickeringGrid.tsx
+++ src/components/FlickeringGrid.tsx
@@ -22,5 +22,20 @@ function FlickeringGrid({
         let sqaures = null;
         let cols = 0;
         let rows = 0;
+
+
+
+        function setup(){
+            const dpr = Math.min(window.devicePixelRatio ||1, 2);
+            const width = container.clientWidth;
+            const height = container.clientHeight;
+
+            canvas.width = width * dpr;
+            canvas.height = height * dpr;
+            canvas.style.width = width + "px";
+            canvas.style.height = height + "px";
+            ctx.setTransform(1, 0, 0, 1, 0, 0);
+            ctx.scale(dpr, dpr);
+        }
     })
 }
