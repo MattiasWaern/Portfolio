d9a801ddef94d5f02588f9ed133805b07e9a09f9
ReggeMeister <80971552+MattiasWaern@users.noreply.github.com>
2026-09-13 16:20:42 +0200

    försöker fixa magic ui

--- src/pages/Startsida.tsx
+++ src/pages/Startsida.tsx
@@ -1,5 +1,6 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import { FlickeringGrid } from "@/components/magicui/flickering-grid";
 
 
 function HomePage() {
@@ -9,7 +10,16 @@ function HomePage() {
         <Header/>
 
         <main>
-            
+            <section className="relative h-[500px] w-full overflow-hidden">
+                <FlickeringGrid
+                    className="absolute inset-0 z-0 size-full"
+                    squareSize={4}
+                    gridGap={6}
+                    color="#6B7280"
+                    maxOpacity={0.5}
+                    flickerChance={0.1}
+                />
+            </section>
         </main>
 
 
