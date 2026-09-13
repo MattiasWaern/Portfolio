005ea2cfb1e571fe31f185fea09a637c67b9a5a1
ReggeMeister <80971552+MattiasWaern@users.noreply.github.com>
2026-09-13 17:13:49 +0200

    commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,52 +1,40 @@
-
 import "../styles/Header.css";
 import { Link } from "react-router-dom";
-import { FaGithub } from "react-icons/fa";
 import FlickeringGrid from "../components/FlickeringGrid";
 
 function Header() {
-    return (
-        <header>
-            <FlickeringGrid
-                squareSize={2}
-                gridGap={6}
-                flickerChance={0.3}
-                color="110, 231, 183"
-                maxOpacity={0.5}
-            />
+  return (
+    <header>
+      <FlickeringGrid
+        squareSize={4}
+        gridGap={6}
+        flickerChance={0.3}
+        color="120, 310, 220"
+        maxOpacity={0.3}
+      />
 
-            <div className="navbar">
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
+      <div className="navbar">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
 
-                    <li>
-                        <Link to="/Projects">Projects</Link>
-                    </li>
+          <li>
+            <Link to="/Projects">Projects</Link>
+          </li>
 
-                    <li>
-                        <Link to="/Contact">Contact</Link>
-                    </li>
+          <li>
+            <Link to="/Contact">Contact</Link>
+          </li>
 
-                    <li>
-                        <Link to="/About">About</Link>
-                    </li>
+          <li>
+            <Link to="/About">About</Link>
+          </li>
 
-                    <li>
-                        <a
-                            href="https://github.com/MattiasWaern"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <FaGithub />
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </header>
-    );
+        </ul>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
-
