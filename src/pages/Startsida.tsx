69c366fdcec60c770bc4ff322861e75bd2781938
ReggeMeister <80971552+MattiasWaern@users.noreply.github.com>
2026-09-13 16:27:47 +0200

    commit

--- src/pages/Startsida.tsx
+++ src/pages/Startsida.tsx
@@ -1,6 +1,5 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import { FlickeringGrid } from "@/components/magicui/flickering-grid";
 
 
 function HomePage() {
@@ -10,16 +9,7 @@ function HomePage() {
         <Header/>
 
         <main>
-            <section className="relative h-[500px] w-full overflow-hidden">
-                <FlickeringGrid
-                    className="absolute inset-0 z-0 size-full"
-                    squareSize={4}
-                    gridGap={6}
-                    color="#6B7280"
-                    maxOpacity={0.5}
-                    flickerChance={0.1}
-                />
-            </section>
+            
         </main>
 
 
