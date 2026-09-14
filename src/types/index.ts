c87aba008200026343c1033b9a74a55754a0da61
ReggeMeister <80971552+MattiasWaern@users.noreply.github.com>
2026-09-14 22:00:08 +0200

    flyttade över till egen type fil

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+import type { ReactNode } from 'react';
+
+export interface Project {
+    id: number;
+    name: string;
+    description: string;
+    skills: ReactNode[];
+    link: string;
+    image: string;
+}
