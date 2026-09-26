a1f93bf4d5f2efb967245fd1a39220fadbe15bd0
ReggeMeister <80971552+MattiasWaern@users.noreply.github.com>
2026-09-26 16:32:55 +0200

    Update ProjectsData.tsx

--- src/data/ProjectsData.tsx
+++ src/data/ProjectsData.tsx
@@ -1,26 +1,23 @@
-import { FaGithub } from "react-icons/fa";
 import type { Project } from '../types/index';
 
-const ProjectsData = [
-    {
-        id: 1,
-        name: "Project 1",
-        description: "A simple project to demonstrate the concept.",
-        skills: ["JavaScript", "React", "CSS", "HTML", <FaGithub/>, <FaGithub/>, <FaGithub/>],
-        image: "project1.png",
-        link: ""
-    },
 
-    {
-        id: 2,
-        name: "Project 2",
-        description: "Another project to showcase different skills.",
-        skills: ["Python", "Django", "PostgreSQL"],
-        image: "project2.png",
-        link: ""
-    }
-]
-
-
+const ProjectsData: Project[] = [
+  {
+    id: 1,
+    name: 'Project 1',
+    description: 'A simple project to demonstrate the concept.',
+    skills: ['JavaScript', 'React', 'CSS', 'HTML', 'GitHub'],
+    image: 'project1.png',
+    link: '',
+  },
+  {
+    id: 2,
+    name: 'Project 2',
+    description: 'Another project to showcase different skills.',
+    skills: ['Python', 'Django', 'PostgreSQL'],
+    image: 'project2.png',
+    link: '',
+  },
+];
 
 export default ProjectsData;
